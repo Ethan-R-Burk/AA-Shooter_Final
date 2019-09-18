546a17ce8afa36f03f6f05770f1dc12124aec118
Ethan-R-Burk <54331592+Ethan-R-Burk@users.noreply.github.com>
2019-09-18 13:23:57 -0500

    Initial commit

---
+++ Assets/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int NailCount;
+
+    public Text text;
+
+    void Start ()
+    {
+        NailCount = 0;
+    }
+
+    void Update ()
+    {
+        text.text = "High Score: " + NailCount.ToString();
+    }
+}
